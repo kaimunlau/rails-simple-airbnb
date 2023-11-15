84dc85672f7ea1e9f3a62d4caa9097955e62ade3
David Lau <david.kaimun.lau@gmail.com>
2023-11-15 15:26:20 +0100

    Allow user to view all flats.

---
+++ db/migrate/20231115140530_add_img_url_to_flats.rb
@@ -0,0 +1,5 @@
+class AddImgUrlToFlats < ActiveRecord::Migration[7.1]
+  def change
+    add_column :flats, :img_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,14 @@ Flat.destroy_all
 
 puts 'Seeding db...'
 10.times do
+  housing_type = %w[house apartments mansion castle].sample
   flat = Flat.create!(
-    name: "#{Faker::JapaneseMedia::StudioGhibli.unique.character} #{%w[house apartments mansion castle].sample}",
+    name: "#{Faker::JapaneseMedia::StudioGhibli.unique.character} #{housing_type.capitalize}",
     address: Faker::Address.unique.full_address,
     description: Faker::Lorem.paragraph(sentence_count: 2),
     price_per_night: rand(50..500),
-    number_of_guests: rand(1..10)
+    number_of_guests: rand(1..10),
+    img_url: "https://source.unsplash.com/random/#{rand(300..400)}x#{rand(200..300)}?#{housing_type}"
   )
   puts "Created #{flat.name}"
 end
