b05563d667ceeba1c7725e742470bb7bd473800e
David Lau <david.kaimun.lau@gmail.com>
2023-11-15 16:38:07 +0100

    Allow user to edit a flat.

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,19 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to @flat
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def flat_params
